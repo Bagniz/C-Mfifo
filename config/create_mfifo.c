623eff4445471d7edfc3c54617fd01e7c171446c
Bagniz <salimkhengui@gmail.com>
2020-05-10 06:14:05 +0200

    Updated config folder

--- config/create_mfifo.c
+++ config/create_mfifo.c
@@ -7,15 +7,16 @@ int main(int argc, char **argv){
     // Variables
     mfifo *fifo;
 
-    if(argc > 2){
-        // Create a new mfifo object
+    if(argc > 2 && argc < 5){
         if(argv[3] != NULL && strcmp(argv[3], "true") == 0)
+            // Create a new mfifo object with O_EXCL option
             fifo = mfifo_connect(argv[1], O_CREAT | O_EXCL, 0666, atoi(argv[2]));
         else
+            // Create a new mfifo object
             fifo = mfifo_connect(argv[1], O_CREAT, 0666, atoi(argv[2]));
             
         if(fifo == NULL){
-            printf("Error, maybe check argument values\n");
+            printf("Error, maybe check argument values or Mfifo object \033[1;34m%s\033[0m already exists\n", argv[1]);
         }
         else{
             printf("Mfifo object \033[1;34m%s\033[0m created with capacity \033[1;34m%ld\033[0m\n", argv[1], mfifo_capacity(fifo));
--- config/delete_mfifo.c
+++ config/delete_mfifo.c
@@ -4,13 +4,13 @@
 #include <string.h>
 
 int main(int argc, char **argv){
-    if(argc > 1){
-            
+    if(argc == 2){
+        // Delete the mfifo object
         if(mfifo_unlink(argv[1]) != 0){
-            printf("Error, maybe check argument values\n");
+            printf("Error, maybe check argument values or Mfifo object \033[1;34m%s\033[0m does not exist\n", argv[1]);
         }
         else{
-            printf("Mfifo object %s deleted\n", argv[1]);
+            printf("Mfifo object \033[1;34m%s\033[0m deleted\n", argv[1]);
         }
     }
     else{
